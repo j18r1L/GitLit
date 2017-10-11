f8e93001b9f18cba2bc9a7a1d27de2b56aef3565
Марк <Mark@Kusik.local>
2017-10-11 18:19:44 +0300

    Create Xcode project

---
+++ GitLit/GitLit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitLit
+//
+//  Created by Марк on 11.10.17.
+//  Copyright © 2017 GitLit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
