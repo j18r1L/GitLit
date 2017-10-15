7712f27bb67e6cc8df44a7e22b5dd051b3fe2c93
emilastanov <emila1998@me.com>
2017-10-15 13:14:32 +0300

    Another version slide menu

---
+++ GitLit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GitLit
+//
+//  Created by Emil Astanov on 15.10.17.
+//  Copyright © 2017 Emil Astanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
